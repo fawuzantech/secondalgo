c17c491d60a69a57aef92709c72ac2aa794857b1
fawuzantech <ibrahimpima76@gmail.com>
2025-04-25 00:26:10 +0000

    Implement code changes to enhance functionality and improve performance

--- app/welcome/landing.tsx
+++ app/welcome/landing.tsx
@@ -1,22 +1,64 @@
 import { NavLink } from "react-router"
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/24/solid';
+
+const features = [
+  {
+    name: 'Push to deploy.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Database backups.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+  },
+];
 
 export function Landing() {
-return (
-    <>
-   <main className="min-h screen flex justify-center align-center">
-    <header className=" flex flex-col items center  justify-center gap-6  p-3">
-      {/* <nav className="flex gap-6 ">
-    </nav> */}
-    </header>
-</main>
-   <div className="flex flex-col justify-center align-center items-center">
-    <div>
-      <h1 className="  flex justify-center align-center items-center mt-35 text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-black to-purple-500 text-shadow-3d transform perspective-1000 rotate-x-15 hover:rotate-x-0 transition-transform duration-300 animate-pulse">You Just Need the Second Algorithym </h1> 
-         </div>
-        <button className="mt-9 w-40 h-15 cursor-pointer text-2xl bg-amber-100  text-black backdrop-blur-2xl p-4">
-          <NavLink to="https://paystack.com/buy/secondalgotiktok">Get it</NavLink>
-        </button>
-   </div>
-  </>
-)
-}
+  return (
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pt-4 lg:pr-8">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                A better workflow
+              </p>
+              {/* <p className="mt-6 text-lg/8 text-gray-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+              </p> */}
+              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-900">
+                      <feature.icon className="absolute top-1 left-1 w-6 h-6 text-indigo-600" aria-hidden="true" />
+                      {feature.name}
+                    </dt>{' '}
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <img
+            alt="Product screenshot"
+            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+            width={2432}
+            height={1442}
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
